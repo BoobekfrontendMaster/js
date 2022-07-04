0e6d2a113ad6ce840788903a64d401b3445b5d60
Boobek Kalmatov <boobekkalmatov8@gmail.com>
2022-07-04 14:26:34 +0300

    начала проекта

---
+++ js/main.js
@@ -0,0 +1,148 @@
+// let isReady = true ? console.log('все готово') : console.log('все не готово');
+
+
+
+// const arr = [3,8,12];
+// const even = (elem) => elem % 2 === 0;
+
+// console.log(arr.some(even));
+
+
+// const is = number => {
+//   const even = number % 2 === 1;
+//   console.log(even);
+// }
+
+// is(4)
+// is(7)
+// is(3)
+// is(8)
+
+// let num = 5;
+// let strnum = '10'
+// console.log(num+++strnum);
+
+// function A() {}
+// function B() {}
+
+// A.prototype = B.prototype = {}
+// let a = new A()
+
+// console.log(a instanceof B);
+
+// const name ='name'
+// age = 21;
+
+// console.log(delete name);
+// console.log(delete age);
+
+// let jsdz = prompt('Склько будет 2 + 2 / 2?');
+
+// if(jsdz == 2) {
+//   console.log('провильно');
+// }else if(jsdz ==  'thwoo') {
+//   console.log('провильно');
+// }else {
+//   console.log('вы тупой');
+// }
+
+// let jsdz = prompt('сколько вам лет?');
+
+// if(jsdz <= 3) {
+//   console.log('идите куда поведут');
+// }else if(jsdz < 10) {
+//   console.log('проситесь на дутскую плашадку');
+// }else if(jsdz == 11) {
+//   console.log('вам в школу пара');
+// }else {
+//   console.log('пошел вон');
+// }
+
+
+
+
+// var isDone = 0;
+// var b = false;
+// var num = 
+
+
+// let object = {
+//     a: 1,
+//     b: 2,
+//     c: function () {this.c},
+// }
+
+// console.log(object.c(   ));
+
+
+// let test = new Array();
+// let test2 = new Set()
+// console.log(test, test2);
+
+
+// var b = 10;
+
+// if(b % 3 == 0) {
+//     var i = 10
+// }
+
+
+// console.log(i);
+
+
+// var a = 5;
+
+// var test = 5 != a ? 'yes': 'no'
+
+// console.log(test);
+
+// let b = 'str';
+// let a = undefined;
+
+// console.log(b ?? a );
+
+// function nework
+
+
+// let str =  function () {
+//     let strFun = 'strFun';
+// }
+
+// console.log(strFun);
+
+// let yrTest = prompt('что вы хотите?');
+
+// switch (yrTest) {
+//     case 'морожонное':
+//             let aiskrim = prompt('какую морожонное вы хотите?');
+
+//             if(aiskrim === 'шоколадное') {
+//                 console.log('вот ваше шоколадное');
+//             }else if(aiskrim === 'клубничное'){
+//                 console.log('вот ваше клубничное');
+//             }else {
+//                 console.log('нет такого');
+//             }
+//         break;
+//             case 'phone' : 
+//                 let phone = prompt('какоу модель телефота вы хотите?')
+//                 if(phone === 'apple') {
+//                     console.log('вот ваше apple');
+//                 }else if(phone === 'sumsung') {
+//                     console.log('вот ваше sumsung');
+//                 }else {
+//                     console.log('нет такого');
+//                 };break;
+//     default: console.log('нет такого (простите)');
+//         break;
+// };
+
+let arr = ['hello', 'world'];
+console.log(arr);
+console.log(arr.length = 0);
+
+
+// let number = 123232;
+// let string = 'hello'
+
+
